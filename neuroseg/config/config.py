31ece20e0e51c6c6636878829cf896e84942922a
Filippo Castelli <castelli@lens.unifi.it>
2020-09-22 16:15:06 +0200

    basic cfg parsing

--- neuroseg/config/config.py
+++ neuroseg/config/config.py
@@ -23,8 +23,80 @@ class TrainingConfig:
         self.loss = cfg_dict["loss"]
         self.track_metrics = cfg_dict["track_metrics"]
         
+        # model parsing
+        self.model_cfg = cfg_dict["model_cfg"]
+        self._parse_model_cfg(self.model_cfg)
         
+        # data augmentation parsing
+        self.da_cfg = cfg_dict["data_augmentation_cfg"]
+        self._parse_data_augmentation_cfg(self.da_cfg)
+        
+        # performance evaluation parsing
+        self.pe_cfg = cfg_dict["performance_evaluation_cfg"]
+        self._parse_performance_evaluation_cfg(self.pe_cfg)
+        
+        # callbacks parsing
+        self.callbacks_cfg = cfg_dict["callbacks_cfg"]
+        self._parse_callbacks_cfg(self.callbacks_cfg)
+        
+        # notes parsing
+        self.notes = cfg_dict["notes"]
     
+    
+    # > MODEL PARSING <
+    def _parse_model_cfg(self, model_cfg):
+        if self.training_mode == "2d":
+            if model_cfg["model"] == "unet2d":
+                self._parse_unet2d_cfg(model_cfg)
+            elif model_cfg["model"] == "unet3d":
+                self._parse_unet3d_cfg(model_cfg)
+            else:
+                raise NotImplementedError(model_cfg["model"])
+        else:
+            raise NotImplementedError(self.training_mode)
+              
+    def _parse_unet_base(self, model_cfg):
+        self.model = model_cfg["model"]
+        self.crop_shape = model_cfg["crop_shape"]
+        self.unet_depth = model_cfg["unet_depth"]
+        self.base_filters = model_cfg["base_filters"]
+        self.batch_normalization = model_cfg["batch_normalization"]
+        self.residual_preactivation = model_cfg["residual_preactivation"]
+        self.transposed_convolution = model_cfg["transposed_convolution"]
+   
+    def _parse_unet2d_cfg(self, model_cfg):
+        self._parse_unet_base(model_cfg)
+        
+    def _parse_unet3d_cfg(self, model_cfg):
+        self._parse_unet_base(model_cfg)
+    
+    # > DATA AUGMENTATION PARSING <
+    def _parse_data_augmentation_cfg(self, da_cfg):
+        if self.training_mode == "2d":
+            self._parse_data_augmentation_cfg_2d(da_cfg)
+        else:
+            raise NotImplementedError(self.training_mode)
+    
+    def _parse_data_augmentation_cfg_2d(self, da_cfg):
+        self.da_debug_mode = da_cfg["debug_mode"]
+        self.da_single_thread = da_cfg["single_thread"]
+        self.da_threads = da_cfg["threads"]
+        
+        self.da_transform_cfg = da_cfg["transform_cfg"]
+        self.da_transforms = list(self.da_transform_cfg.keys())
+        
+    # > PERFORMANCE EVALUATION PARSING <
+    def _parse_performance_evaluation_cfg(self, pe_cfg):
+        self.pe_batchsize = pe_cfg["batchsize"]
+        self.pe_chunksize = pe_cfg["chunksize"]
+        self.pe_classification_threashold = pe_cfg["classification_threshold"]
+        self.pe_add_empty_channel = pe_cfg["add_empty_channel"]
+        
+        
+    # > CALLBACK PARSING <
+    def _parse_callbacks_cfg(self, callbacks_cfg):
+        self.callbacks = list(callbacks_cfg.keys())
+        
     def save(self):
         pass
     
@@ -62,6 +134,10 @@ class TrainingConfig:
     
     
 if __name__ == "__main__":
+    import platform
     
-    cfg_path = Path("/home/phil/repos/neuroseg/neuroseg/config/ex_cfg.yml")
+    if platform.system() == "Windows":
+        cfg_path = Path("C:/Users/filip/Documenti/GitHub/neuroseg/neuroseg/config/ex_cfg.yml")
+    else:
+        cfg_path = Path("/home/phil/repos/neuroseg/neuroseg/config/ex_cfg.yml")
     config = TrainingConfig(cfg_path)
