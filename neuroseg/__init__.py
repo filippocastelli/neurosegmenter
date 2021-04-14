bc7a6a36322e034729c12f1b91bb992093530724
Filippo Maria Castelli <castelli@lens.unifi.it>
2021-04-14 20:43:50 +0200

    version file included in package and relative import fixed

--- neuroseg/__init__.py
+++ neuroseg/__init__.py
@@ -1,8 +1,15 @@
+import inspect
+from pathlib import Path
+
 from neuroseg.training import train
 from neuroseg.predict import predict
 from neuroseg.config  import TrainConfig
 from neuroseg.config import PredictConfig
 
-with open("neuroseg/version", "r") as fh:
+import neuroseg
+version_path = Path(inspect.getfile(neuroseg)).parent.joinpath("version")
+
+with version_path.open(mode="r") as fh:
 	__version__ = fh.read()
+    
 #__version__ = "0.0.1"
