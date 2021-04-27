389c8afac525f9d554c5184403dc177498247a18
Filippo Maria Castelli <castelli@lens.unifi.it>
2021-04-27 17:30:53 +0200

    parametrizable weighted crossentropy

--- neuroseg/config/metrics_cfg.py
+++ neuroseg/config/metrics_cfg.py
@@ -1,5 +1,5 @@
 from neuroseg.metrics import jaccard_index, dice_coefficient, weighted_cross_entropy_loss
-from functools import partial
+
 
 class MetricsConfigurator:
 
@@ -10,12 +10,10 @@ class MetricsConfigurator:
         self.track_metrics_names = self.config.track_metrics
         self.loss_name = self.config.loss
 
-        self.class_weights = self.config.class_weights
         self.pos_weight = self.config.pos_weight
 
         self.track_metrics = self._get_track_metrics(self.track_metrics_names)
         self.loss = self._get_metric(self.loss_name)
-
 
     def _get_metric(self, metric_name):
         SUPPORTED_METRICS = {
@@ -23,7 +21,7 @@ class MetricsConfigurator:
             "dice_coefficient": dice_coefficient,
             "binary_crossentropy": "binary_crossentropy",
             "accuracy": "accuracy",
-            "weighted_binary_crossentropy": partial(weighted_cross_entropy_loss, pos_weight=self.pos_weight)
+            "weighted_binary_crossentropy": weighted_cross_entropy_loss(self.pos_weight)
         }
 
         if metric_name in SUPPORTED_METRICS:
@@ -31,10 +29,9 @@ class MetricsConfigurator:
         else:
             raise NotImplementedError(metric_name)
 
-    @classmethod
-    def _get_track_metrics(cls, track_metric_list):
+    def _get_track_metrics(self, track_metric_list):
         metric_list = []
         for track_metric_name in track_metric_list:
-            metric = cls._get_metric(track_metric_name)
+            metric = self._get_metric(track_metric_name)
             metric_list.append(metric)
         return metric_list
--- neuroseg/metrics/losses.py
+++ neuroseg/metrics/losses.py
@@ -1,10 +1,14 @@
 import tensorflow as tf
 
 
-def weighted_cross_entropy_loss(y_true, y_pred, pos_weight):
+def weighted_cross_entropy_loss(pos_weight):
     if pos_weight is None:
         raise ValueError("need to define a pos_weight parameter in training config to use weighted_cross_entropy_loss")
-    y_pred = tf.clip_by_value(y_pred, tf.keras.backend.epsilon(), 1 - tf.keras.backend.epsilon())
-    p_hat = tf.math.log(y_pred / (1 - y_pred))
-    loss = tf.nn.weighted_cross_entropy_with_logits(logits=p_hat, targets=y_true, pos_weight=pos_weight)
-    return tf.reduce_mean(loss)
+
+    def loss(y_true, y_pred):
+        y_pred = tf.clip_by_value(y_pred, tf.keras.backend.epsilon(), 1 - tf.keras.backend.epsilon())
+        p_hat = tf.math.log(y_pred / (1 - y_pred))
+        loss = tf.nn.weighted_cross_entropy_with_logits(logits=p_hat, labels=y_true, pos_weight=pos_weight)
+        return tf.reduce_mean(loss)
+
+    return loss
