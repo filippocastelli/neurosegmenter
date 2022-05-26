73a7f20a74e97dc2045ceeb2a41ea6ddf3ca2d75
Filippo Maria Castelli <filippocastelli42@gmail.com>
2022-05-26 14:49:57 +0200

    example train script now accepts -c argument

--- examples/train.py
+++ examples/train.py
@@ -1,13 +1,19 @@
 from pathlib import Path
+from wandb import Config
 import yaml
+from argparse import ArgumentParser
 
 from neuroseg import TrainConfig, train
 
-print(Path.cwd())
-config_path = Path("config_2d_single_images.yml")
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("-c", "--config", type=str, required=True, dest="config_path", help="config_path")
+    
+    args = parser.parse_args()
+    config_path = Path(args.config_path)
 
-with config_path.open(mode="r") as infile:
-    yaml.load(infile, Loader=yaml.FullLoader)
-config = TrainConfig(config_path)
+    config = TrainConfig(yml_path=config_path)
+    train(config)
 
-train(config)
+if __name__ == "__main__":
+    main()
