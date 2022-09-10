d5d6b12ab5934e2a587399aef835d191341214ce
Filippo Maria Castelli <filippocastelli42@gmail.com>
2022-09-10 12:37:51 +0200

    cache

--- neuroseg/utils/multipagetiff.py
+++ neuroseg/utils/multipagetiff.py
@@ -1,14 +1,15 @@
+import chunk
 from pathlib import Path
 import numpy as np
 import collections
+import logging
 
 from PIL import ImageSequence, Image
 import multiprocessing
 from multiprocessing.managers import DictProxy
 from tqdm import tqdm
-from functools import partial, lru_cache
+from functools import partial, lru_cache, cached_property
 import time
-
 
 class MultiPageTIFF:
     """A simple class for multithreaded reading of multipage TIFFs"""
@@ -16,6 +17,7 @@ class MultiPageTIFF:
                     img_path:Path,
                     n_threads: int = None,
                     multiproc=True,
+                    min_chunk_size: int = 4,
                     lru_cache_limit=200):
         self.img_path = img_path
         self.img = Image.open(str(img_path))
@@ -25,7 +27,12 @@ class MultiPageTIFF:
         self._n_threads = multiprocessing.cpu_count() if n_threads is None else n_threads
         self._lru_cache_limit = lru_cache_limit
         self._lru_deque = collections.deque([], maxlen=self._lru_cache_limit)
+        self.min_chunk_size = min_chunk_size
     
+    @cached_property
+    def shape(self):
+        return (self.img.n_frames,) + self.img.size[::-1]
+
     @staticmethod
     def _get_plane(
         idx: int,
@@ -36,8 +43,13 @@ class MultiPageTIFF:
         return arr
     
     def _get_cached_elems(self, idxs: list) -> dict:
-        return {arr_idx: self._cachedict[arr_idx] for idx, arr_idx in enumerate(idxs) if arr_idx in self._cachedict}
-    
+        
+        cached_dict = {arr_idx: self._cachedict[arr_idx] for idx, arr_idx in enumerate(idxs) if arr_idx in self._cachedict}
+        if cached_dict != {}:
+            print(f"Found {len(cached_dict)} cached slices")
+
+        return cached_dict
+
     def _update_lru(self, idxs: list):
         for idx in idxs:
             if idx in self._lru_deque:
@@ -56,15 +68,23 @@ class MultiPageTIFF:
         _get_plane_part = partial(self._get_plane, data_path=self.img_path)
         cached_elems_dict = self._get_cached_elems(idxs)
         non_cached_idxs = [arr_idx for arr_idx in idxs if arr_idx not in cached_elems_dict]
-
+        
         if multiproc and (len(non_cached_idxs)>1):
-            print(idxs)
+            # print(idxs)
             start = time.time()
             non_cached_planes = []
+
             n_proc = min(self._n_threads, len(non_cached_idxs))
+            if len(non_cached_idxs) // self.min_chunk_size < n_proc:
+                chunk_size = self.min_chunk_size
+                n_proc = len(non_cached_idxs) // self.min_chunk_size
+            else:
+                chunk_size = len(non_cached_idxs) // n_proc
+
             pool = multiprocessing.Pool(processes=n_proc)
             with pool as p:
-                non_cached_planes=list(tqdm(p.map(_get_plane_part, non_cached_idxs), total=len(non_cached_idxs)))
+                print(f"Reading {len(non_cached_idxs)} slices with {n_proc} processes with chunk size {chunk_size}")
+                non_cached_planes=list(tqdm(p.map(_get_plane_part, non_cached_idxs, chunksize=chunk_size), total=len(non_cached_idxs)))
             print(f"Time: {time.time() - start}")
         else:
             start = time.time()
@@ -87,7 +107,7 @@ class MultiPageTIFF:
     def __getitem__(self, val: slice):
         # case img[0]
         if isinstance(val, int):
-            return self._get_plane(val)
+            return self._get_planes_range(val, val+1, multiproc=self.multiproc)
         # case img[0:10]
         elif isinstance(val, slice):
             return self._get_planes_range(val.start, val.stop, multiproc=self.multiproc)
